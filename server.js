8a8d1d9f695b93994fc76b0d65b6a75a59e97375
Antoine-Flo <antoine.flouzat@gmail.com>
2020-12-12 18:03:01 +0100

    before slider

--- server.js
+++ server.js
@@ -10,13 +10,24 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
-
-
 app.use(express.static('./public'));
+
+
 app.get('/', function (req, res) {
     res.render('pages/home')
 });
 
+app.get('/contact', (req, res) => {
+    res.render('pages/contact')
+})
+
+app.get('/about', (req, res) => {
+    res.render('pages/about')
+})
+
+app.get('/portfolio', (req, res) => {
+    res.render('pages/portfolio')
+})
 
 
 app.listen(port, () => console.log(`Server started on port ${port}!`));
